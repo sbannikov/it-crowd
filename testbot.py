d0e446dc7a866bd7b2aa435b754734670f73a4d1
kon3gor <37112953+kon3gor@users.noreply.github.com>
2018-03-09 15:33:19 +0300

    Update testbot.py

--- testbot.py
+++ testbot.py
@@ -10,7 +10,8 @@ app = fl(__name__)
 
 def refresh_lp(vk):
     # Запрашиваем параметры подключения
-    return vk.messages.getLongPollServer()
+    vk = v
+    return v.messages.getLongPollServer()
 def main():
     session = vk.Session( '64539d85290a5d7f2bd416abe68d12e626efbf1eb275c51643b02751130c917190a717638ed758c9ba1fa')
     vk = vk.API(session, v='5.38')
